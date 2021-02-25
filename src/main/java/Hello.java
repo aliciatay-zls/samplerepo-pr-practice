ec7c416126fa7ecde5b07a5e329a732014a4138f
aliciatay-zls <e0424736@u.nus.edu>
2021-02-25 12:57:46 +0800

    Add print current date feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+
 public class Hello {
+    public static LocalDate getDate() {
+        LocalDate today = LocalDate.now();
+        return today;
+    }
+
     public static void main(String[] args) {
+        System.out.println("Date: " + getDate());
         System.out.println("Message: hello world");
     }
 }
